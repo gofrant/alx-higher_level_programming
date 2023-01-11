cb1e6d9ac2e99c5d03e954b33a43bb20b55fb588
gofrant <gofran178@gmail.com>
2023-01-11 10:29:36 -0800

    v

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = [x for x in my_list]
-    for x in range(len(new)):
-        if new[x] == search:
-            new[x] = replace
-            return new
+    new = [replace if x == search else x for x in my_list]
+    return new
